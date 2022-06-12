98da3e5413a7b0ccf63e2c92343df6d774ee2c4f
mparrag <miklos.d.parrag@gmail.com>
2022-06-12 15:44:48 +0200

    second day resolved + d1/2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import Days.Day1;
+import Days.Day2;
 
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello 2015 Advent!!!");
         new Day1();
+        new Day2();
     }
 }
