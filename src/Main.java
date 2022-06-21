dfa43012dc3f1f51a35bdfaf4d1bde6a3a5caf30
mparrag <miklos.d.parrag@gmail.com>
2022-06-21 15:49:23 +0200

    eleventh day resolved

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,16 @@ import Days.*;
 class Main {
     public static void main(String[] args){
         System.out.println("Hello 2015 Advent!!!");
-//        new Day1();
-//        new Day2();
-//        new Day3();
-//        new Day4();
-//        new Day5();
-//        new Day6();
-//        new Day7();
-//        new Day8();
-//        new Day9();
-//        new Day10();
+        new Day1();
+        new Day2();
+        new Day3();
+        new Day4();
+        new Day5();
+        new Day6();
+        new Day7();
+        new Day8();
+        new Day9();
+        new Day10();
         new Day11();
     }
 }
