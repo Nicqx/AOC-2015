62dfe9fe786227769c32bc9a7a4c4c08a1ab81f8
Nicqx <miklos.d.parrag@gmail.com>
2022-07-11 19:23:49 +0200

    day 2 optimize

--- src/Days/Day2.java
+++ src/Days/Day2.java
@@ -4,28 +4,30 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import static java.lang.Math.min;
 import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 public class Day2 {
     private static int ribbon = 0;
-    private static String text;
 
     public Day2() {
-        try (Scanner scanner = new Scanner(new File("resources/D2/input"))) {
-            int surface = 0;
+        System.out.println("D2 - The elves need to order this amount: " + fileReader("resources/D2/input"));
+        System.out.println("D2/2 - The elves used for the ribbon this length: " + ribbon);
+    }
+
+    private int fileReader(String res) {
+        int surface = 0;
+        try (Scanner scanner = new Scanner(new File(res))) {
             while (scanner.hasNext()) {
                 surface += process(scanner.nextLine());
             }
-            System.out.println("D2 - The elves need to order this ammount: " + surface);
-            System.out.println("D2/2 - The elves used for the ribbon this length: " + ribbon);
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
         }
+        return surface;
     }
 
     private static int process(String text) {
-        Day2.text = text;
         int a = Integer.parseInt(text.split("x")[0]);
         int b = Integer.parseInt(text.split("x")[1]);
         int c = Integer.parseInt(text.split("x")[2]);
