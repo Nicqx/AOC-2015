c864e4583df8a1018da96cb863687baa32810d6f
Nicqx <miklos.d.parrag@gmail.com>
2022-07-12 15:35:32 +0200

    day 23 optimize

--- src/Days/Day23.java
+++ src/Days/Day23.java
@@ -8,90 +8,123 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day23 {
-    int a = 0;
-    int b = 0;
-    int nextCoord = 0;
-    ArrayList<String> register = new ArrayList<>();
+    Registers registers;
+
+    ArrayList<String> commandList = new ArrayList<>();
 
     public Day23() {
+        fileReader("resources/D23/input");
+        calculateB(0);
+        System.out.println("D23 - the value in register b: " + registers.getB());
+        calculateB(1);
+        System.out.println("D23/2 - the value in register b: " + registers.getB());
+    }
 
-        try (Scanner scanner = new Scanner(new File("resources/D23/input"))) {
+    private void calculateB(int a) {
+        registers = new Registers(a, 0);
+        boolean found;
+        do {
+            found = process(registers);
+        } while (found);
+    }
+
+    private void fileReader(String res) {
+        try (Scanner scanner = new Scanner(new File(res))) {
             while (scanner.hasNext()) {
-                register.add(scanner.nextLine());
-
+                commandList.add(scanner.nextLine());
             }
-
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
         }
-        boolean result;
-        do {
-            result = process(nextCoord);
-        } while (result);
-        System.out.println("D23 - the value in register b: " + b);
-        a=1;
-        b=0;
-        nextCoord = 0;
-        do {
-            result = process(nextCoord);
-        } while (result);
-        System.out.println("D23/2 - the value in register b: " + b);
     }
 
-    private boolean process(int coord) {
-        if (register.size() < coord + 1 || coord < 0) {
+    private boolean process(Registers registers) {
+        if (commandList.size() < registers.getNextCoord() + 1 || registers.getNextCoord() < 0) {
             return false;
         }
-        if (register.get(coord).equals("inc a")) {
-            a++;
-            nextCoord = coord + 1;
+        if (commandList.get(registers.getNextCoord()).equals("inc a")) {
+            registers.setA(registers.getA() + 1);
+            registers.setNextCoord(registers.getNextCoord() + 1);
             return true;
         }
-        if (register.get(coord).equals("inc b")) {
-            b++;
-            nextCoord = coord + 1;
+        if (commandList.get(registers.getNextCoord()).equals("inc b")) {
+            registers.setB(registers.getB() + 1);
+            registers.setNextCoord(registers.getNextCoord() + 1);
             return true;
         }
-        if (register.get(coord).equals("hlf a")) {
-            a /= 2;
-            nextCoord = coord + 1;
+        if (commandList.get(registers.getNextCoord()).equals("hlf a")) {
+            registers.setA(registers.getA() / 2);
+            registers.setNextCoord(registers.getNextCoord() + 1);
             return true;
         }
-        if (register.get(coord).equals("tpl a")) {
-            a *= 3;
-            nextCoord++;
+        if (commandList.get(registers.getNextCoord()).equals("tpl a")) {
+            registers.setA(registers.getA() * 3);
+            registers.setNextCoord(registers.getNextCoord() + 1);
             return true;
         }
-        if (register.get(coord).startsWith("jmp")) {
-            String num = register.get(coord).split(" ")[1];
+        if (commandList.get(registers.getNextCoord()).startsWith("jmp")) {
+            String num = commandList.get(registers.getNextCoord()).split(" ")[1];
             if (num.startsWith("+")) {
-                nextCoord += Integer.parseInt(num.split("\\+")[1]);
-                return true;
+                registers.setNextCoord(registers.getNextCoord() + Integer.parseInt(num.split("\\+")[1]));
             } else {
-                nextCoord -= Integer.parseInt(num.split("-")[1]);
-                return true;
+                registers.setNextCoord(registers.getNextCoord() - Integer.parseInt(num.split("-")[1]));
             }
+            return true;
         }
-        Matcher matcher = Pattern.compile("jie a, \\+(\\d+)").matcher(register.get(coord));
+        Matcher matcher = Pattern.compile("jie a, \\+(\\d+)").matcher(commandList.get(registers.getNextCoord()));
         if (matcher.find()) {
-            if (a % 2 == 0) {
-                nextCoord += Integer.parseInt(matcher.group(1));
-                return true;
+            if (registers.getA() % 2 == 0) {
+                registers.setNextCoord(registers.getNextCoord() + Integer.parseInt(matcher.group(1)));
             } else {
-                nextCoord++;
-                return true;
+                registers.setNextCoord(registers.getNextCoord() + 1);
             }
+            return true;
         }
-        matcher = Pattern.compile("jio a, \\+(\\d+)").matcher(register.get(coord));
+        matcher = Pattern.compile("jio a, \\+(\\d+)").matcher(commandList.get(registers.getNextCoord()));
         if (matcher.find()) {
-            if (a == 1) {
-                nextCoord += Integer.parseInt(matcher.group(1));
-                return true;
+            if (registers.getA() == 1) {
+                registers.setNextCoord(registers.getNextCoord() + Integer.parseInt(matcher.group(1)));
             } else {
-                nextCoord++;
-                return true;
+                registers.setNextCoord(registers.getNextCoord() + 1);
             }
+            return true;
         }
         return true;
+    }
+
+    static class Registers {
+        int a;
+        int b;
+
+        int nextCoord = 0;
+
+        public Registers(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int getA() {
+            return a;
+        }
+
+        public void setA(int a) {
+            this.a = a;
+        }
+
+        public int getB() {
+            return b;
+        }
+
+        public void setB(int b) {
+            this.b = b;
+        }
+
+        public int getNextCoord() {
+            return nextCoord;
+        }
+
+        public void setNextCoord(int nextCoord) {
+            this.nextCoord = nextCoord;
+        }
     }
 }
