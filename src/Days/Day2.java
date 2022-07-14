fd03c5f17d228a56e88a5479f40d1c571a4d0b38
Nicqx <miklos.d.parrag@gmail.com>
2022-07-14 20:56:23 +0200

    day 2 optimize for builder

--- src/Days/Day2.java
+++ src/Days/Day2.java
@@ -2,39 +2,84 @@ package Days;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 
 public class Day2 {
-    private static int ribbon = 0;
+    private final ArrayList<Box> boxes = new ArrayList<>();
 
     public Day2() {
-        System.out.println("D2 - The elves need to order this amount: " + fileReader("resources/D2/input"));
-        System.out.println("D2/2 - The elves used for the ribbon this length: " + ribbon);
+        fileReader("resources/D2/input");
+        System.out.println("D2 - The elves need to order this amount: " + processWrap());
+        System.out.println("D2/2 - The elves used for the ribbon this length: " + processRibbon());
     }
 
-    private int fileReader(String res) {
-        int surface = 0;
+    private void fileReader(String res) {
         try (Scanner scanner = new Scanner(new File(res))) {
             while (scanner.hasNext()) {
-                surface += process(scanner.nextLine());
+                String line = scanner.nextLine();
+                boxes.add(new Box.BoxBuilder(Integer.parseInt(line.split("x")[0]), Integer.parseInt(line.split("x")[1]), Integer.parseInt(line.split("x")[2])).build());
             }
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
         }
-        return surface;
     }
 
-    private static int process(String text) {
-        int a = Integer.parseInt(text.split("x")[0]);
-        int b = Integer.parseInt(text.split("x")[1]);
-        int c = Integer.parseInt(text.split("x")[2]);
-        int sideAB = a * b;
-        int sideBC = b * c;
-        int sideAC = a * c;
-        ribbon += ((a + b + c - max(a, max(b, c))) * 2 + (a * b * c));
-        return (2 * sideAB) + (2 * sideAC) + (2 * sideBC) + min(sideAB, min(sideAC, sideBC));
+    private int processRibbon() {
+        int completeRibbonLength = 0;
+        for (Box box : boxes) {
+            completeRibbonLength += box.getRibbonNeed();
+        }
+        return completeRibbonLength;
+    }
+
+    private int processWrap() {
+        int completeSurface = 0;
+        for (Box box : boxes) {
+            completeSurface += box.getWrapNeed();
+        }
+        return completeSurface;
+    }
+
+    public static class Box {
+        private final int a;
+        private final int b;
+        private final int c;
+
+        public int getRibbonNeed() {
+            return ((a + b + c - max(a, max(b, c))) * 2 + (a * b * c));
+        }
+
+        public int getWrapNeed() {
+            int sideAB = a * b;
+            int sideBC = b * c;
+            int sideAC = a * c;
+            return (2 * sideAB) + (2 * sideAC) + (2 * sideBC) + min(sideAB, min(sideAC, sideBC));
+        }
+
+        private Box(BoxBuilder builder) {
+            this.a = builder.a;
+            this.b = builder.b;
+            this.c = builder.c;
+        }
+
+        public static class BoxBuilder {
+            private final int a;
+            private final int b;
+            private final int c;
+
+            public BoxBuilder(int a, int b, int c) {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+
+            public Box build() {
+                return new Box(this);
+            }
+        }
     }
 }
